4994549cca451d07aa4c0d9781d659b6467249f7
Patrik <psirb@myuax.com>
2024-05-23 16:50:17 +0200

    Endpoint de alumnos

--- src/views/codemasters/v1/alumnos/index.js
+++ src/views/codemasters/v1/alumnos/index.js
@@ -1,8 +1,25 @@
 'use strict'
 
 import { Router } from 'express'
+import { Router } from 'express'
+import AlumnoController from '../../../controllers/codemasters/v1/alumnos'
 let router = Router()
 
-// TODO
+
+// Obtenemos todos los alumnos
+
+router.get('/', AlumnoController.getAllAlumnos)
+
+// Seleccionamos a un alumno por su id
+router.get('/:id', AlumnoController.getAlumnoById)
+
+// Creamos un nuevo alumno
+router.post('/', AlumnoController.createAlumno)
+
+// Actualizamos un alumno
+router.put('/:id', AlumnoController.updateAlumno)
+
+// Eliminamos un alumno
+router.delete('/:id', AlumnoController.deleteAlumno)
 
 export default router
